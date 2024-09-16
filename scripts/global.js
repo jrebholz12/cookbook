439e6439aa86bfec8bd864445140838517593896
Justin Rebholz <jrebholz12@icloud.com>
2024-09-16 11:58:31 -0500

    changed null lastname

--- scripts/global.js
+++ scripts/global.js
@@ -20,7 +20,7 @@ function sortTabs(page, style){
 
 function getLastName(){
   let name = localStorage.getItem('lastName')
-  if(name === ''){
+  if(name === null){
     return
   } else {
     document.getElementById('familyName').innerHTML = name
