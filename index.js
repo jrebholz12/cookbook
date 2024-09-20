cd21926b265a593837739ae949b5956f9938130a
Justin Rebholz <jrebholz12@icloud.com>
2024-09-20 11:48:18 -0500

    first commit utilizing firestore and react

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnAzn18yGCVuWLUCx3KVC9Sf656MicZ38",
+  authDomain: "rebzlist.firebaseapp.com",
+  projectId: "rebzlist",
+  storageBucket: "rebzlist.appspot.com",
+  messagingSenderId: "319668133045",
+  appId: "1:319668133045:web:bf7bc4fc3d1781db75f2fc",
+  measurementId: "G-48186NQYRC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
